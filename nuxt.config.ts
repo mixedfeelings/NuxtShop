6293ddbd8fa5feae856cc918b037c9d5f5c890ea
George Wietor <george@grcmc.org>
2025-10-15 03:53:14 -0400

    add nitro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,40 +1,40 @@
 import { defineNuxtConfig } from "nuxt3";
 
 export default defineNuxtConfig({
-  publicRuntimeConfig: {
-    SHOPIFY_STOREFRONT_ENDPOINT: process.env.SHOPIFY_STOREFRONT_ENDPOINT,
-    SHOPIFY_STOREFRONT_ACCESS_TOKEN:
-      process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
-  },
-  buildModules: ["@pinia/nuxt", "@vueuse/nuxt"],
-  alias: {
-    colorMode: "@nuxtjs/color-mode",
-  },
-  colorMode: {
-    preference: 'system', // default value of $colorMode.preference
-    fallback: 'light', // fallback value if not system preference found
-    hid: 'nuxt-color-mode-script',
-    globalName: '__NUXT_COLOR_MODE__',
-    componentName: 'ColorScheme',
-    classPrefix: '',
-    classSuffix: '-mode',
-    storageKey: 'nuxt-color-mode'
-  },
-  build: {
-    transpile: [
-      "@apollo/client",
-      "@vue/apollo-composable",
-      "ts-invariant/process",
-    ],
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
-    },
-  },
-  css: ["~/assets/css/fonts.css", "~/assets/css/global.css"],
+	publicRuntimeConfig: {
+		SHOPIFY_STOREFRONT_ENDPOINT: process.env.SHOPIFY_STOREFRONT_ENDPOINT,
+		SHOPIFY_STOREFRONT_ACCESS_TOKEN:
+			process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+	},
+	buildModules: ["@pinia/nuxt", "@vueuse/nuxt"],
+	alias: {
+		colorMode: "@nuxtjs/color-mode",
+	},
+	nitro: { externals: { inline: ["tslib", "graphql-tag"] } },
+	colorMode: {
+		preference: "system", // default value of $colorMode.preference
+		fallback: "light", // fallback value if not system preference found
+		hid: "nuxt-color-mode-script",
+		globalName: "__NUXT_COLOR_MODE__",
+		componentName: "ColorScheme",
+		classPrefix: "",
+		classSuffix: "-mode",
+		storageKey: "nuxt-color-mode",
+	},
+	build: {
+		transpile: [
+			"@apollo/client",
+			"@vue/apollo-composable",
+			"ts-invariant/process",
+		],
+		postcss: {
+			postcssOptions: {
+				plugins: {
+					tailwindcss: {},
+					autoprefixer: {},
+				},
+			},
+		},
+	},
+	css: ["~/assets/css/fonts.css", "~/assets/css/global.css"],
 });
-
