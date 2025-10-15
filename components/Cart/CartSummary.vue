947b2dd6274bfc8b0c5055f224f94ef9e2522855
George Wietor <george@grcmc.org>
2025-10-15 00:12:45 -0400

    update cart summary messages

--- components/Cart/CartSummary.vue
+++ components/Cart/CartSummary.vue
@@ -1,28 +1,31 @@
 <template>
-  <div class="pb-4 mb-4 border-b-2 border-black">
-    <div class="flex justify-between">
-      <p>Subtotal:</p>
-      <p>
-        {{ subtotalAmount }}
-      </p>
-    </div>
-    <div class="flex justify-between">
-      <p>Estimated Tax:</p>
-      <p>
-        {{ totalTaxAmount }}
-      </p>
-    </div>
-    <div v-if="totalDutyAmount" class="flex justify-between">
-      <p>Duties:</p>
-      <p>
-        {{ totalDutyAmount }}
-      </p>
-    </div>
-    <div class="flex justify-between">
-      <p>Estimated Total:</p>
-      <p>{{ totalAmount }}</p>
-    </div>
-  </div>
+	<div class="pb-4 mb-4 border-b-2 border-black">
+		<div class="flex justify-between">
+			<p>Subtotal:</p>
+			<p>
+				{{ subtotalAmount }}
+			</p>
+		</div>
+		<div class="flex justify-between" v-if="totalTaxAmount">
+			<p>Estimated Tax:</p>
+			<p>
+				{{ totalTaxAmount }}
+			</p>
+		</div>
+		<div v-if="totalDutyAmount" class="flex justify-between">
+			<p>Duties:</p>
+			<p>
+				{{ totalDutyAmount }}
+			</p>
+		</div>
+		<div class="flex justify-between" v-if="totalTaxtAmount || totalDutyAmount">
+			<p>Estimated Total:</p>
+			<p>{{ totalAmount }}</p>
+		</div>
+		Shipping
+		{{ !totalTaxAmount || !totalDutyAmount ? "and tax" : "" }} calculated at
+		checkout.
+	</div>
 </template>
 
 <script setup lang="ts">
@@ -31,5 +34,5 @@ import { useCartStore } from "~/stores/cart";
 
 const cartStore = useCartStore();
 const { subtotalAmount, totalTaxAmount, totalDutyAmount, totalAmount } =
-  storeToRefs(cartStore);
+	storeToRefs(cartStore);
 </script>
