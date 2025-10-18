69028a5d0ed0a462033779d98da013d3a6a9ff6b
George Wietor <george@grcmc.org>
2025-10-18 19:34:26 -0400

    had sr-only h1

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,26 @@
 <template>
-  <section class="py-8">
-    <Html>
-      <Head>
-        <Title>Issue Press</Title>
-        <Meta name="description" content="Issue Press is a tiny publisher and Risograph print shop based in Grand Rapids, Michigan" />
-        <Meta property="og:title" content="Issue Press" />
-        <Meta property="og:site_name" content="Issue Press" />
-        <Meta property="og:type" content="website" />
-        <Meta property="og:url" content="https://issue.press/" />          
-        <Meta property="og:image" content="/issue-press.png" />
-        <Meta name="twitter:card" content="summary" />
-        <Meta name="twitter:site" content="@issuepress" /> 
-      </Head>
-    </Html>
+	<section class="py-8">
+		<Html>
+			<Head>
+				<Title>Issue Press</Title>
+				<Meta
+					name="description"
+					content="Issue Press is a tiny publisher and Risograph print shop based in Grand Rapids, Michigan"
+				/>
+				<Meta property="og:title" content="Issue Press" />
+				<Meta property="og:site_name" content="Issue Press" />
+				<Meta property="og:type" content="website" />
+				<Meta property="og:url" content="https://issue.press/" />
+				<Meta property="og:image" content="/issue-press.png" />
+				<Meta name="twitter:card" content="summary" />
+				<Meta name="twitter:site" content="@issuepress" />
+			</Head>
+		</Html>
 
+		<h1 class="sr-only">Issue Press</h1>
 
-    <!-- <template>
-  <ColorScheme placeholder="..." tag="span">
-    Color mode: <b>{{ $colorMode.preference }}</b>
-    <span v-if="$colorMode.preference === 'system'">(<i>{{ $colorMode.value }}</i> mode detected)</span>
-  </ColorScheme>
-</template> -->
-
-    <FeaturedProducts
-      collectionHandle="in-stock"
-      :numberProducts="12"
-    />
-
-  </section>
+		<FeaturedProducts collectionHandle="in-stock" :numberProducts="12" />
+	</section>
 </template>
 
 <script setup>
@@ -36,15 +29,20 @@ import { useShopStore } from "~/stores/shop";
 import { useColorStore } from "~/stores/colors";
 
 const colorStore = useColorStore();
-const colorMode = useColorMode()
+const colorMode = useColorMode();
 
 const shopStore = useShopStore();
 const { description } = storeToRefs(shopStore);
 
 onMounted(() => {
-  colorStore.setGlobalColor();
-  document.documentElement.style.setProperty('--global-color',`var(--color-${colorStore.globalColor})`);
-  document.documentElement.style.setProperty('--global-text-color',`var(--color-${colorStore.globalTextColor})`);
+	colorStore.setGlobalColor();
+	document.documentElement.style.setProperty(
+		"--global-color",
+		`var(--color-${colorStore.globalColor})`
+	);
+	document.documentElement.style.setProperty(
+		"--global-text-color",
+		`var(--color-${colorStore.globalTextColor})`
+	);
 });
-
 </script>
