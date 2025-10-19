2aa2297ed06c6706509a7dd9d1abf7a9b0f4f471
George Wietor <george@grcmc.org>
2025-10-18 23:25:26 -0400

    add a footer

--- app.vue
+++ app.vue
@@ -1,18 +1,17 @@
-<template v-if="colorStore.globalColor" >
-  <Header />
-  <Cart />
-  <main class="min-h-[75vh]">
-    <div
-      v-if="
-        config.SHOPIFY_STOREFRONT_ENDPOINT &&
-        config.SHOPIFY_STOREFRONT_ACCESS_TOKEN
-      "
-    >
-      <NuxtPage />
-    </div>
-
-  </main>
-  <!-- <Footer /> -->
+<template v-if="colorStore.globalColor">
+	<Header />
+	<Cart />
+	<main class="min-h-[75vh]">
+		<div
+			v-if="
+				config.SHOPIFY_STOREFRONT_ENDPOINT &&
+				config.SHOPIFY_STOREFRONT_ACCESS_TOKEN
+			"
+		>
+			<NuxtPage />
+		</div>
+	</main>
+	<Footer />
 </template>
 
 <script lang="ts" setup>
@@ -20,63 +19,61 @@ import "~/assets/css/tailwind.css";
 import { useShopStore } from "~/stores/shop";
 
 useMeta({
-  htmlAttrs: {
-    lang: "en",
-  },
-  link: [
-    {
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon.ico',
-      sizes: 'any',
-    },
-      {
-      rel: 'icon',
-      type: 'image/svg+xml',
-      href: '/icon.svg',
-    },
-    {
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon-128.png',
-    },
-    {
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon-192.png',
-    },
-    {
-      rel: 'apple-touch-icon',
-      type: 'image/x-icon',
-      href: '/favicon-180.png',
-    },
-  ],  
-  script: [
-    {
-      src: 'https://www.googletagmanager.com/gtag/js?id=UA-22429627-1', 
-      async: true,
-      type: 'text/javascript'
-    },
-    {
-      children: `
+	htmlAttrs: {
+		lang: "en",
+	},
+	link: [
+		{
+			rel: "icon",
+			type: "image/x-icon",
+			href: "/favicon.ico",
+			sizes: "any",
+		},
+		{
+			rel: "icon",
+			type: "image/svg+xml",
+			href: "/icon.svg",
+		},
+		{
+			rel: "icon",
+			type: "image/x-icon",
+			href: "/favicon-128.png",
+		},
+		{
+			rel: "icon",
+			type: "image/x-icon",
+			href: "/favicon-192.png",
+		},
+		{
+			rel: "apple-touch-icon",
+			type: "image/x-icon",
+			href: "/favicon-180.png",
+		},
+	],
+	script: [
+		{
+			src: "https://www.googletagmanager.com/gtag/js?id=UA-22429627-1",
+			async: true,
+			type: "text/javascript",
+		},
+		{
+			children: `
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
       
         gtag('config', 'UA-22429627-1');
       `,
-      type: 'text/javascript'
-    }
-  ],
+			type: "text/javascript",
+		},
+	],
 });
 
 // Global Store Actions
 const { pending, error } = await useAsyncData("shop", () => {
-  const shopStore = useShopStore();
-  return shopStore.getShopGlobals();
+	const shopStore = useShopStore();
+	return shopStore.getShopGlobals();
 });
 
 const config = useRuntimeConfig();
-
-
 </script>
--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -1,9 +1,56 @@
 <template>
-  <footer>
-    
-  </footer>
+	<footer class="p-6 border-t-2">
+		<div class="">
+			<p class="pt-2 text-center text-sm">
+				<em
+					>Independent <NuxtLink to="/shop">artist publishing</NuxtLink> &
+					<NuxtLink to="/printing">Risograph printing</NuxtLink> from Grand
+					Rapids, Michigan</em
+				>
+			</p>
+			<ul
+				class="flex flex-col md:flex-row justify-center md:items-center gap-x-6 gap-y-2 leading-none text-xs leading-none flex-wrap"
+			>
+				<li class="flex items-center justify-center gap-1">
+					<span class="text-base leading-none" name="copyright">©</span>
+					2011–{{ new Date().getFullYear() }}
+					<NuxtLink to="/">Issue Press</NuxtLink>
+				</li>
+				<li
+					v-for="(policy, key) in availablePolicies"
+					:key="key"
+					class="flex justify-center"
+				>
+					<NuxtLink
+						:to="`/policies/${toKebab(key)}`"
+						class="underline hover:no-underline"
+					>
+						{{ policy.title }}
+					</NuxtLink>
+				</li>
+			</ul>
+		</div>
+	</footer>
 </template>
 
 <script setup lang="ts">
-import HeartIcon from "@heroicons/vue/solid/HeartIcon.js";
+import { computed } from "vue";
+import { useQuery, useResult } from "@vue/apollo-composable";
+import { policies } from "~~/apollo/queries/policies";
+
+// fetch shop policies
+const { result } = useQuery(policies);
+const policiesData = useResult(result, null, (data) => data.shop);
+
+// helper: camelCase → kebab-case
+const toKebab = (str: string) =>
+	str.replace(/([a-z0-9])([A-Z])/g, "$1-$2").toLowerCase();
+
+// filter out null policies
+const availablePolicies = computed(() => {
+	if (!policiesData.value) return {};
+	return Object.fromEntries(
+		Object.entries(policiesData.value).filter(([_, val]) => val && val.title)
+	);
+});
 </script>
